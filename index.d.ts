4a588a7c6a70822493e74871785600f801c611af
Simon Garnotel <simon.garnotel@gmail.com>
2025-05-05 11:11:13 +0200

    fix types and bad export

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,12 @@
 import { NextRequest } from 'next/server'
 
-import { Session } from '@/defs/index.d'
-
+import { Session } from '@/defs'
 import { FindUser, ValidatePassword } from '@/lib/strategy/index'
 
 export declare const APILoginRoute = async (req: NextRequest) =>
-  Response['json']
-export declare const APILogoutRoute = async () => Response['json']
-export declare const APIRefreshSessionRoute = async () => Response['json']
+  Response['json']()
+export declare const APILogoutRoute = async () => Response['json']()
+export declare const APIRefreshSessionRoute = async () => Response['json']()
 export declare const getSession = async () => Session
 export declare const setLocaLStrategy = async (
   findUser: FindUser,
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
 import { loginRoute } from '@/app/api/login'
 import { logoutRoute } from '@/app/api/logout'
-import { getSession } from '@/lib/session'
 import { refreshSessionRoute } from '@/app/api/refreshSession'
 
+import { getSession as getSession0 } from '@/lib/session'
 import { setLocaLStrategy as setLocaLStrategy0 } from '@/lib/strategy'
 
 // API routes
 export const APILoginRoute = loginRoute
 export const APILogoutRoute = logoutRoute
 export const APIRefreshSessionRoute = refreshSessionRoute
+
+// Session
+export const getSession = getSession0
 
 // Strategy
 export const setLocaLStrategy = setLocaLStrategy0
