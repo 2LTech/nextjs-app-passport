a88350205f773bb36975b547be7623ef204ee60c
Simon Garnotel <simon.garnotel@gmail.com>
2025-05-05 10:52:31 +0200

    0.9.4

--- index.d.ts
+++ index.d.ts
@@ -4,9 +4,10 @@ import { Session } from '@/defs/index.d'
 
 import { FindUser, ValidatePassword } from '@/lib/strategy/index'
 
-export declare const APILoginRoute = async (req: NextRequest) => Response
-export declare const APILogoutRoute = async () => Response
-export declare const APIRefreshSessionRoute = () => Response
+export declare const APILoginRoute = async (req: NextRequest) =>
+  Response['json']
+export declare const APILogoutRoute = async () => Response['json']
+export declare const APIRefreshSessionRoute = async () => Response['json']
 export declare const getSession = async () => Session
 export declare const setLocaLStrategy = async (
   findUser: FindUser,
