8d8abc5b3a0ba599fe9213fab0252966191aa6f4
Simon Garnotel <simon.garnotel@gmail.com>
2025-05-05 10:30:02 +0200

    udpate & type

--- index.d.ts
+++ index.d.ts
@@ -4,14 +4,14 @@ import { Session } from '@/defs/index.d'
 
 import { FindUser, ValidatePassword } from '@/lib/strategy/index'
 
-export declare const APILoginRoute = (req: NextRequest) => Promise<Response>
-export declare const APILogoutRoute = () => Promise<Response>
-export declare const APIRefreshSessionRoute = () => Promise<Response>
-export declare const getSession = () => Promise<Session>
-export declare const setLocaLStrategy = (
+export declare const APILoginRoute = async (req: NextRequest) => Response
+export declare const APILogoutRoute = async () => Response
+export declare const APIRefreshSessionRoute = () => Response
+export declare const getSession = async () => Session
+export declare const setLocaLStrategy = async (
   findUser: FindUser,
   validatePassword: ValidatePassword
-) => Promise<any>
+) => any
 
 declare const NodeAppPassport = {
   APILoginRoute,
